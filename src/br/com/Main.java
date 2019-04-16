9f1d38e2d5f4d614455b4c9a46a88de96cc51cae
percivalli <thamuz.khaos@gmail.com>
2019-04-15 21:51:48 -0300

    Initial commit

---
+++ src/br/com/Main.java
@@ -0,0 +1,39 @@
+package br.com;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(5);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(8);
+        lista.add(8);
+
+        for (Integer lixo: lista){
+            System.out.println(lixo);
+        }
+
+        Set<Integer> conjunto = new HashSet<>();
+        conjunto.add(1);
+        conjunto.add(5);
+        conjunto.add(5);
+        conjunto.add(6);
+        conjunto.add(7);
+        conjunto.add(8);
+        conjunto.add(8);
+        conjunto.add(8);
+
+        for (Integer valor: conjunto) {
+            System.out.println(valor);
+        }
+    }
+}
